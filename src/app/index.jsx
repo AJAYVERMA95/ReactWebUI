4568bfe1e06a056c50b78b84959744dce3d7181a
AJAY VERMA <verma.av.ajay@gmail.com>
2017-09-28 01:40:24 +0530

    semantic css added

--- src/app/index.jsx
+++ src/app/index.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
+
+
 import HomePage from "./components/pages/HomePage.jsx";
 import LoginPage from "./components/pages/LoginPage.jsx";
 
@@ -10,7 +12,7 @@ class App extends React.Component {
     render() {
         return (
             <Router>
-                <div>
+                <div className = "ui container">
                     <Route path="/" exact component={HomePage} />
                     <Route path="/login" exact component={LoginPage} />
                 </div>
