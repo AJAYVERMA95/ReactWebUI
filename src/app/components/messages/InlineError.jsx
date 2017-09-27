be1d2434ff486b1dafaf8eb126495fd215e1da43
AJAY VERMA <verma.av.ajay@gmail.com>
2017-09-28 03:03:24 +0530

    Login form working with some client side validation

---
+++ src/app/components/messages/InlineError.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class InlineError extends React.Component {
+    render() {
+        return (
+            <span style={{ color: "red" }}>{this.props.text}</span>
+        );
+    }
+}
+
+module.exports = InlineError;
--- src/app/components/pages/LoginPage.jsx
+++ src/app/components/pages/LoginPage.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
 
+import LoginForm from '../forms/LoginForm.jsx';
+
 class LoginPage extends React.Component {
+    
+    submit(data){
+        console.log(data);
+    }
+    
     render(){
         return(
             <div>
                 <h1>Login Page</h1> 
+                <LoginForm submit={this.submit.bind(this)}/>
             </div>
         );
     }
